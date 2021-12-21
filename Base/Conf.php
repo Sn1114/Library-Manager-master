c0ccc48f876cd30837bc77e0ab457dbc3fa713d1
Sn1114 <93084373+Sn1114@users.noreply.github.com>
2021-12-21 12:03:07 +0800

    Add files via upload

---
+++ Base/Conf.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    //数据库配置
+    'db_host'   =>  "106.15.186.34",
+    'db_user'   =>  "root",
+    'db_pwd'    =>  "zjicm#2019",
+    'db_name'   =>  "205701123",
+    'db_port'   =>  "3306",
+    'charset'   =>  "utf8",
+
+    //默认路由配置
+    'default_plantform'   => "Common",
+    'default_controller'  => "Login",
+    'default_action'      => "index"
+);
